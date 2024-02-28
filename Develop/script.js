25ed4d06462d2c56f99dfa4fce721f1995751139
afry72 <ajfrye72@gmail.com>
2024-02-28 16:24:27 -0500

    added base code, still needs a lot of work

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(passwordLength, addLowercase, addUppercase, addNumbers, addSymbols) {
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbersChars = "1234567890";
+  var symbolsChars = "!@#$%^&*()-_=+[{]};:',<.>/?";
+
+
+  var addedChars = "";
+  var password = "";
+
+  addedChars += addLowercase ? lowercaseChars:"";
+  addedChars += addUppercase ? uppercaseChars:"";
+  addedChars += addNumbers ? numbersChars:"";
+  addedChars += addSymbols ? symbolsChars:"";
+  
+  console.log(addedChars);
+
+  return '';
+}
+
+
+var passwordLength = 15;
+var addLowercase = true;
+var addUppercase = true;
+var addNumbers = true;
+var addSymbols = true; 
+  
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);

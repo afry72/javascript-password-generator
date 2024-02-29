bb00025c42dae5d54455a611fd568fb7e0708cac
afry72 <ajfrye72@gmail.com>
2024-02-29 00:08:42 -0500

    attempting to fix boilerplate bug

--- Develop/script.js
+++ Develop/script.js
@@ -8,6 +8,7 @@ var addNumbers = true;
 var addSymbols = true; 
 
 function generatePassword() {
+  //setting up arrays for each individual characters that could be used
   var lowercaseChars = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
   var uppercaseChars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
   var numbersChars = ["1","2","3","4","5","6","7","8","9","0"];
@@ -17,6 +18,7 @@ function generatePassword() {
   var addedChars = [];
   var password = "";
   
+  //creating individual for loops to make the output as random as possible
   if (addLowercase) {
     for(var i = 0; i < lowercaseChars.length; i++) {
       addedChars.push(lowercaseChars[i]);
@@ -40,19 +42,19 @@ function generatePassword() {
       addedChars.push(symbolsChars[i]);
     }
   } 
-
+  //creating if statements that limit the amount of charactors to 8-128 and making sure you have to pick atleast 1 character type
   if(addedChars.length <= 0){
-  alert("password must have atleast 1 charactor type");
+  alert("password must have atleast 1 character type");
   return null;
   }
 
   if(passwordLength > 128){
-    alert("password must be less then 128 charactors");
+    alert("password must be less then 128 characters");
     return null;
   }
     
   if(passwordLength < 8){
-    alert("password must be more than 8 charactors");
+    alert("password must be more than 8 characters");
     return null;
   }
 
@@ -65,6 +67,7 @@ function generatePassword() {
   return password;
 }
   
+//sending the user prompts to decide how they want to customize their password
 function passwordDetails() {
   passwordLength = prompt("how long do you want your password");
   addLowercase = confirm("do you want to include lower case letters");
@@ -74,7 +77,7 @@ function passwordDetails() {
 }
 // Write password to the #password input
 function writePassword() {
-  //call a function to set charactor booleans with window.alert
+  //call a function to set character booleans with window.alert
   passwordDetails();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ### JAVASCRIPT PASSWORD GENERATOR
 
+This password generator has been an amazing project for me and i cant wait to put this to good use. This password generator will randomly generate an array of letters, numbers, and symbols at any length between 8 and 128. these passwords are extremely secure and you will mathematically never get the same array twice.
+
 ## CHALLENGES
+
+This challenge proved to be extremely difficult. trying to figure out if i should use strings or arrays and trying to decide how i should best randomize the results. pure randomness has always been a problem in computer science but i figured using individual for loops for each character type made it just random enough without introducing a mountain of code.
 ## USES
+
+this is extremely useful for someone who uses different passwords for every single thing or someone who rotates passwords on a regular basis without any bias 
